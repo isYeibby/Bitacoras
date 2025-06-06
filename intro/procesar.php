8e1581db5593758855a9512c04d93d95baae1f83
Yhosmar <sYeibbyS@outlook.com>
2025-06-06 16:58:07 -0600

    Subida de archivos desde local

--- intro/procesar.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Manejo de formularios
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST['nombre'];
-    echo "Hola, $nombre";
-}
-?>
--- php/variables.php
+++
@@ -1,9 +0,0 @@
-<?php
-// variables
-$nombre = "Carlos";
-$edad = 25;
-$precio = 19.99;
-$esEstudiante = true;
-// Mostrar variables
-echo "Nombre: $nombre, Edad: $edad, Precio: $precio, ¿estudiante?: $esEstudiante";
-?>
